639ca504536742654a91f8ba031b383bf4dc4140
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-04 14:22:40 +0300

    add basic implementation of tracer

---
+++ Tracer/Core/Tracer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tracer.Core
+{
+    public class Tracer : ITracer
+    {
+        private Stopwatch _stopWatch;
+        public Tracer()
+        {
+            _stopWatch = new Stopwatch();
+        }
+
+        public void StartTrace()
+        {
+            StackTrace stackTrace = new StackTrace();
+            _stopWatch.Reset();
+            _stopWatch.Start();
+        }
+
+        public void StopTrace()
+        {
+            _stopWatch.Stop();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame? sf = st.GetFrame(1);
+
+            return new TraceResult(sf.GetMethod().Name, sf.GetMethod().DeclaringType.Name, _stopWatch.ElapsedMilliseconds);
+        }
+    }
+}
--- Tracer/Program.cs
+++ Tracer/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Tracer.Core
+{
+    class Program
+    {
+        static void Main()
+        {
+            Tracer tracer = new Tracer();
+            C test = new C(tracer);
+
+            test.M0();
+            
+        }
+
+        public class C
+        {
+            private ITracer _tracer;
+
+            public C(ITracer tracer)
+            {
+                _tracer = tracer;
+            }
+
+            public void M0()
+            {
+                M1();
+                M2();
+            }
+
+            private void M1()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(100);
+                _tracer.StopTrace();
+                TraceResult res = _tracer.GetTraceResult();
+                Console.WriteLine(res.Time);
+            }
+
+            private void M2()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(200);
+                _tracer.StopTrace();
+                TraceResult res = _tracer.GetTraceResult();
+                Console.WriteLine(res.Time);
+            }
+        }
+    }
+}
