94f919f5c09f76b7ef1a4e87eb3ddef1150b1807
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-10 21:42:12 +0300

    implement json serializer

--- Tracer.Serialization/Json/JsonTraceResultSerializer.cs
+++ Tracer.Serialization/Json/JsonTraceResultSerializer.cs
@@ -1,7 +1,18 @@
-﻿using Tracer.Serialization.Abstractions;
+﻿using System.Text.Json;
+using System.Text;
+using Tracer.Serialization.Abstractions;
+using Tracer.Core.Models;
+
 namespace Json
 {
     public class JsonTraceResultSerializer : ITraceResultSerializer
     {
+        public string Extension { get; } = "json";
+
+        public void Serialize(TraceResult traceResult, Stream to)
+        {
+            var res = JsonSerializer.Serialize<TraceResult>(traceResult);
+            to.Write(Encoding.Default.GetBytes(res));
+        }
     }
 }
