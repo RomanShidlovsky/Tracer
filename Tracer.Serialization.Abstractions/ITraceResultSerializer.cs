7a4fe5396c5885b0d9ac8677a44f47c93c830a28
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-10 14:51:00 +0300

    add Interface for serializer

---
+++ Tracer.Serialization.Abstractions/ITraceResultSerializer.cs
@@ -0,0 +1,10 @@
+﻿using Tracer.Core.Models;
+
+namespace Tracer.Serialization.Abstractions
+{
+    public interface ITraceResultSerializer
+    {
+        string Extension { get; }
+        void Serialize(TraceResult traceResult, Stream to);
+    }
+}
